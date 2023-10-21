3794146143740bc68be46b1a5184f2982661efa2
Kk2020k <108136211+Kk2020k@users.noreply.github.com>
2023-10-21 13:15:53 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -9,20 +9,20 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', '20389440'))
-    API_HASH = str(getenv('API_HASH', 'a1a06a18eb9153e9dbd447cfd5da2457'))
+    API_ID = int(getenv('API_ID', '17627887'))
+    API_HASH = str(getenv('API_HASH', 'd14e54b0791ca1f8b0f26786439e336e'))
     BOT_TOKEN = str(getenv('BOT_TOKEN', '6564513574:AAGDqUaEmeu0m4DjLDetNc4nooVTWYT7Fzo'))
     name = str(getenv('name', 'filetolinkbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001722984461'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001850979293'))
     PORT = int(getenv('PORT', '8080'))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'kingvj01'))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'Professor22k'))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
@@ -35,9 +35,9 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://sushankm16:4i1WAfPYKWyqPIDD@cluster0.sngp9pz.mongodb.net/?retryWrites=true&w=majority'))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'vj_botz'))
+    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://King:king@file.fuxewu1.mongodb.net/?retryWrites=true&w=majority'))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'Uptate2k'))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split()))     
-    URL_SHORTENR_WEBSITE = getenv('URL_SHORTENR_WEBSITE', 'onepagelink.in')
-    URL_SHORTNER_WEBSITE_API = getenv('URL_SHORTNER_WEBSITE_API', 'c47e1c4469c0a66e74af73153cb8f4d3b304d010')
+    URL_SHORTENR_WEBSITE = getenv('URL_SHORTENR_WEBSITE', 'tnshort.net')
+    URL_SHORTNER_WEBSITE_API = getenv('URL_SHORTNER_WEBSITE_API', '62b1ec93449a6ac2f78d6d16e8cb76227951eac3')
     
